74e3a4b6813ee3be34a025398d29e2ee87b8324c
Mary <mariia.poddubnaya@gmail.com>
2019-09-26 10:53:19 +0300

    Update homework-7.js

--- homework-7.js
+++ homework-7.js
@@ -23,4 +23,35 @@ img.onclick = function (event) {
 }
 
 ///Additional
+var collection = ["main", "section", "article", "div"]
+var titles =  [ "first", "second", "third", "fourth" ]
+var deleted = []
+var container = document.body
+for (var i = 0; i < collection.length; i++) {
+	var elem = container.appendChild(document.createElement(collection[i]))
 
+	elem.setAttribute('title', titles[i])
+
+	elem.style = `
+	background-color: #ff00ff50;
+	border: dotted 1px yellow;
+	padding: 60px;
+	transition: all 0.5s;
+	`
+
+	elem.onclick = function(event) {
+		event.stopPropagation()
+		deleted.push(event.target.parentNode.removeChild(event.target))
+	}
+
+	elem.onmouseover = function(event) {
+		event.stopPropagation()
+		event.target.style.background = '#ffff0050'
+	}
+
+	elem.onmouseout = function(event) {
+		event.target.style.background = '#ff00ff50'
+	}
+
+	container = elem 
+}
